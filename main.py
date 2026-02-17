274267a4d4faf49268f67ff070f3c2c5d70aae6d
Avinash-patnaik <avinashpatnaik1001@gmail.com>
2026-02-17 10:56:01 +0100

    feat: change in the controller

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import messagebox, font
 from src.reader import get_latest_file, rename_file
@@ -21,3 +22,33 @@ class MailerGUI:
     
         tk.Button(root, text="SPESE", width=25, height=2,
                   bg="#153ceb", command=lambda: self.start_process("SPESE")).pack(pady=10)
+        
+    def start_process(self, mode):
+
+        folder_path = f"data/{mode}/"
+
+        target_file = get_latest_file(folder_path)
+
+        if not target_file:
+            messagebox.showwarning("No File", f"No new files found in {folder_path}")
+            return
+
+        # 3. Confirmation Dialog
+        if messagebox.askyesno("Confirm", f"Process latest file: {os.path.basename(target_file)}?"):
+            try:
+                # --- START PIPELINE ---
+                # df = load_excel(target_file)
+                # status = run_mailer_logic(df, mode)
+                # -----------------------
+                
+                # 4. Rename file after success
+                new_name = rename_file(target_file)
+                messagebox.showinfo("Success", f"Emails sent! File renamed to: {new_name}")
+                
+            except Exception as e:
+                messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MailerGUI(root)
+    root.mainloop()
