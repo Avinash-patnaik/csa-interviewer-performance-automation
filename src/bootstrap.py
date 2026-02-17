5adabb87bc505fd2a4daeae96ade71b2ebb40573
Avinash-patnaik <avinashpatnaik1001@gmail.com>
2026-02-17 11:10:49 +0100

    feat: update the bootstrap module in the project

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -1,3 +1,17 @@
 import os 
 import sys
 from dotenv import load_dotenv
+import yaml
+
+
+def load_config():
+
+    load_dotenv()
+
+    with open("config/settings.yaml", "r") as f:
+        config = yaml.safe_load(f)
+
+    config['smtp']['user'] = os.getenv("SMTP_USER")
+    config['smtp']['password'] = os.getenv("SMTP_PASSWORD")
+
+    return config
